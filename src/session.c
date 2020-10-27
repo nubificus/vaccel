039bd4881154819a0650ea8f1b4f71e63bc20060
Babis Chalios <mail@bchalios.io>
2020-10-27 17:25:27 +0100

    Offload session handling to virtio backend
    In cases where we are running inside a guest we want the session
    handling to be offloaded to the host hypervisor/runtime.
    
    To that end, we handle the virtio backend in a "special" way:
    * We only allow a single virtio backend.
    * We require that the virtio backend implements the session handling API.
    * During execution, we track the presence of a virtio backend and
    offload to it session handling operations
    
    Signed-off-by: Babis Chalios <mail@bchalios.io>

--- src/session.c
+++ src/session.c
@@ -1,18 +1,27 @@
 #include "vaccel.h"
+#include "backend.h"
 
 #include <stdint.h>
 
 int vaccel_sess_init(struct vaccel_session *sess, uint32_t flags)
 {
-	(void)sess;
-	(void)flags;
+	/* if we're using virtio as a backend offload the session initialization
+	 * to the host */
+	struct vaccel_backend *virtio = get_virtio_backend();
+	if (virtio)
+		return virtio->vaccel_sess_init(sess, flags);
+
 
 	return VACCEL_OK;
 }
 
 int vaccel_sess_free(struct vaccel_session *sess)
 {
-	(void)sess;
+	/* if we're using virtio as a backend offload the session cleanup to the
+	 * host */
+	struct vaccel_backend *virtio = get_virtio_backend();
+	if (virtio)
+		return virtio->vaccel_sess_free(sess);
 
 	return VACCEL_OK;
 }
