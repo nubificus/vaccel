15494bdf3cabbbacc3d6e4d3140d847a66698925
Babis Chalios <mail@bchalios.io>
2020-11-03 05:18:09 -0600

    Write session id back to user
    The virtio layer is writing back to us the session id in the virtio
    session object. We need to write it back to the user facing object
    
    Signed-off-by: Babis Chalios <mail@bchalios.io>

--- src/plugins/virtio/session.c
+++ src/plugins/virtio/session.c
@@ -9,16 +9,16 @@
 int virtio_sess_init(struct vaccel_session *sess, uint32_t flags)
 {
 	struct accel_session session;
-	struct accel_gen_op_arg args = {
-		sizeof(sess->session_id),
-		(char *)&sess->session_id
-	};
 
 	memset(&session, 0, sizeof(session));
-	session.u.gen.out_nr = 1;
-	session.u.gen.out = &args;
+	int ret = dev_write(ACCIOC_GEN_SESS_CREATE, &session);
+	if (ret)
+		return ret;
 
-	return dev_write(ACCIOC_GEN_SESS_CREATE, &session);
+	/* Write the session id back to user */
+	sess->session_id = session.id;
+
+	return VACCEL_OK;
 }
 
 int virtio_sess_free(struct vaccel_session *sess)
@@ -26,5 +26,6 @@ int virtio_sess_free(struct vaccel_session *sess)
 	struct accel_session session;
 
 	memset(&session, 0, sizeof(session));
+	session.id = sess->session_id;
 	return dev_write(ACCIOC_GEN_SESS_DESTROY, &session);
 }
