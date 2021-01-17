f81b9d4bfea1b0ff4ec9c2c873c763528116c4a5
Anastassios Nanos <ananos@nubificus.co.uk>
2021-01-17 15:08:14 +0000

    Match errno Error codes

--- src/common.h
+++ src/common.h
@@ -2,18 +2,18 @@
 #define __VACCEL_COMMON_H__
 
 /* vaccel return codes */
-#define VACCEL_OK          0
-#define VACCEL_EINVAL      1
-#define VACCEL_ENOMEM      2
-#define VACCEL_ENOTSUP     3
-#define VACCEL_EINPROGRESS 4
-#define VACCEL_EBUSY       5
-#define VACCEL_EEXISTS     6
-#define VACCEL_ENOENT      7
-#define VACCEL_ELIBBAD     8
-#define VACCEL_ENODEV      9
-#define VACCEL_EIO         10
-#define VACCEL_ESESS       11
-#define VACCEL_EBACKEND    12
+#define VACCEL_OK          0	/* All Good :D */
+#define VACCEL_EINVAL      22 	/* EINVAL: Invalid argument */
+#define VACCEL_ENOMEM      13 	/* ENOMEM: Out of memory */
+#define VACCEL_ENOTSUP     95 	/* EOPNOTSUPP: Operation not supported */
+#define VACCEL_EINPROGRESS 115 	/* EINPROGRESS: Operation now in progress */
+#define VACCEL_EBUSY       16 	/* EBUSY: Device or resource busy */
+#define VACCEL_EEXISTS     17 	/* EEXIST: File exists */
+#define VACCEL_ENOENT      2 	/* ENOENT:  No such file or directory */
+#define VACCEL_ELIBBAD     80 	/* ELIBBAD: Accessing a corrupted shared library */
+#define VACCEL_ENODEV      19 	/* ENODEV: No such device */
+#define VACCEL_EIO         5 	/* EIO: I/O error */
+#define VACCEL_ESESS       104	/* ECONNRESET: Connection reset by peer */
+#define VACCEL_EBACKEND    71	/* EPROTO: Protocol error */
 
 #endif /* __VACCEL_COMMON_H__ */
