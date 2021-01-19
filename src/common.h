5c3b9adf072965b3c8c5657e939fad18707f883d
Babis Chalios <mail@bchalios.io>
2021-01-19 15:58:47 +0100

    Fix style
    Signed-off-by: Babis Chalios <mail@bchalios.io>

--- src/common.h
+++ src/common.h
@@ -2,18 +2,18 @@
 #define __VACCEL_COMMON_H__
 
 /* vaccel return codes */
-#define VACCEL_OK          0	/* All Good :D */
-#define VACCEL_EINVAL      22 	/* EINVAL: Invalid argument */
-#define VACCEL_ENOMEM      13 	/* ENOMEM: Out of memory */
-#define VACCEL_ENOTSUP     95 	/* EOPNOTSUPP: Operation not supported */
-#define VACCEL_EINPROGRESS 115 	/* EINPROGRESS: Operation now in progress */
-#define VACCEL_EBUSY       16 	/* EBUSY: Device or resource busy */
-#define VACCEL_EEXISTS     17 	/* EEXIST: File exists */
-#define VACCEL_ENOENT      2 	/* ENOENT:  No such file or directory */
-#define VACCEL_ELIBBAD     80 	/* ELIBBAD: Accessing a corrupted shared library */
-#define VACCEL_ENODEV      19 	/* ENODEV: No such device */
-#define VACCEL_EIO         5 	/* EIO: I/O error */
-#define VACCEL_ESESS       104	/* ECONNRESET: Connection reset by peer */
-#define VACCEL_EBACKEND    71	/* EPROTO: Protocol error */
+#define VACCEL_OK          0   /* All Good :D */
+#define VACCEL_EINVAL      22  /* EINVAL: Invalid argument */
+#define VACCEL_ENOMEM      13  /* ENOMEM: Out of memory */
+#define VACCEL_ENOTSUP     95  /* EOPNOTSUPP: Operation not supported */
+#define VACCEL_EINPROGRESS 115 /* EINPROGRESS: Operation now in progress */
+#define VACCEL_EBUSY       16  /* EBUSY: Device or resource busy */
+#define VACCEL_EEXISTS     17  /* EEXIST: File exists */
+#define VACCEL_ENOENT      2   /* ENOENT:  No such file or directory */
+#define VACCEL_ELIBBAD     80  /* ELIBBAD: Accessing a corrupted shared library */
+#define VACCEL_ENODEV      19  /* ENODEV: No such device */
+#define VACCEL_EIO         5   /* EIO: I/O error */
+#define VACCEL_ESESS       104 /* ECONNRESET: Connection reset by peer */
+#define VACCEL_EBACKEND    71  /* EPROTO: Protocol error */
 
 #endif /* __VACCEL_COMMON_H__ */
