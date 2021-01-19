97e36187e277a55d47db8b91717a7cca291dbe39
Babis Chalios <mail@bchalios.io>
2021-01-19 13:28:29 +0100

    Control logging behaviour from environment
    Add environment variables to control the debug level of logging and the
    file it will be logging to.
    
    Signed-off-by: Babis Chalios <mail@bchalios.io>

--- src/log.c
+++ src/log.c
@@ -1,14 +1,73 @@
 #include "common.h"
 #include "log.h"
 
+#include <stdlib.h>
+#include <string.h>
 #include <slog.h>
 
-static int nEnabledLevels = SLOG_FLAGS_ALL;
-static const char *logfile = "/dev/stdout";
+#define VACCEL_DEBUG_LVL_ERROR 1
+#define VACCEL_DEBUG_LVL_WARN  2
+#define VACCEL_DEBUG_LVL_INFO  3
+#define VACCEL_DEBUG_LVL_DEBUG 4
+
+static void set_debug_level(void)
+{
+	char *env = getenv("VACCEL_DEBUG_LEVEL");
+	if (!env)
+		return;
+
+	int level = atoi(env);
+	int nEnabledLevels = 0;
+	switch (level) {
+		/* FALLTHRU */
+		case VACCEL_DEBUG_LVL_DEBUG:
+			nEnabledLevels |= SLOG_DEBUG;
+		/* FALLTHRU */
+		case VACCEL_DEBUG_LVL_INFO:
+			nEnabledLevels |= SLOG_INFO;
+		/* FALLTHRU */
+		case VACCEL_DEBUG_LVL_WARN:
+			nEnabledLevels |= SLOG_WARN;
+		/* FALLTHRU */
+		case VACCEL_DEBUG_LVL_ERROR:
+			nEnabledLevels |= SLOG_ERROR;
+		default:
+			break;
+	}
+
+	SLogConfig cfg;
+	slog_config_get(&cfg);
+	cfg.nFlags = nEnabledLevels;
+	slog_config_set(&cfg);
+}
+
+static void set_log_file(void)
+{
+	SLogConfig cfg;
+
+	char *env = getenv("VACCEL_LOG_FILE");
+	if (!env)
+		return;
+
+	slog_config_get(&cfg);
+	strncpy(cfg.sFileName, env, SLOG_NAME_MAX);
+
+	if (strncmp(cfg.sFileName, "/dev/stdout", SLOG_NAME_MAX)
+			&& strncmp(cfg.sFileName, "/dev/stderr", SLOG_NAME_MAX)) {
+		cfg.nToFile = 1;
+		cfg.nToScreen = 0;
+	}
+
+	slog_config_set(&cfg);
+}
 
 int vaccel_log_init(void)
 {
-	slog_init(logfile, nEnabledLevels, 0);
+	slog_init("/dev/stdout", 0, 0);
+
+	set_debug_level();
+	set_log_file();
+
 	return VACCEL_OK;
 }
 
