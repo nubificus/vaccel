b8f77475583bb079649b2a9e5f4111e4f095a0b8
Babis Chalios <mail@bchalios.io>
2021-05-06 15:33:43 +0200

    Create `vaccel_file` data structure to assist with resources
    Many of the resources we will be handling need to handle an underlying
    file on persistent storage. We create `struct vaccel_file` as a helper
    data structure to handle reading such a resource from the disk or
    creating it from raw data and persisting it as a resource when needed.
    
    Signed-off-by: Babis Chalios <mail@bchalios.io>

---
+++ src/include/vaccel_file.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <stddef.h>
+#include <stdbool.h>
+
+#include "vaccel_id.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct vaccel_resource;
+
+struct vaccel_file {
+	/* Path to file */
+	char *path;
+
+	/* Do we own the file? */
+	bool path_owned;
+
+	/* Pointer to the contents of the file in case we hold them
+	 * in a buffer */
+	uint8_t *data;
+	size_t size;
+};
+
+int vaccel_file_new(struct vaccel_file *file, const char *path);
+int vaccel_file_from_buffer(
+	struct vaccel_file *file,
+	const uint8_t *buff,
+	size_t size,
+	const char *filename,
+	struct vaccel_resource *res,
+	bool persist
+);
+int vaccel_file_persist(
+	struct vaccel_file *file,
+	struct vaccel_resource *res,
+	const char *filename
+);
+int vaccel_file_destroy(struct vaccel_file *file);
+
+#ifdef __cplusplus
+}
+#endif
