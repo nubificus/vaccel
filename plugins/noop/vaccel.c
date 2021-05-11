82f3a2f7059205c92fdee95d5161b290aa7810ea
Babis Chalios <mail@bchalios.io>
2021-05-11 11:53:02 +0200

    noop: add TensorFlow API
    Dummy implementation for TensorFlow calls to load graph from model and
    run a graph.
    
    Signed-off-by: Babis Chalios <mail@bchalios.io>

--- plugins/noop/vaccel.c
+++ plugins/noop/vaccel.c
@@ -4,6 +4,15 @@
 #include <plugin.h>
 #include <error.h>
 #include <ops/vaccel_ops.h>
+#include <ops/genop.h>
+#include <ops/tf.h>
+#include <tf_model.h>
+
+#define noop_info(fmt, ...) \
+	fprintf(stdout, "[noop] " fmt, ##__VA_ARGS__)
+
+#define noop_error(fmt, ...) \
+	fprintf(stderr, "[noop] " fmt, ##__VA_ARGS__)
 
 static int noop_noop(struct vaccel_session *sess)
 {
@@ -84,6 +93,89 @@ static int noop_exec(struct vaccel_session *sess, const char *library,
 	return VACCEL_OK;
 }
 
+static int noop_tf_model_load_graph(
+	struct vaccel_session *session,
+	struct vaccel_tf_model *model,
+	struct vaccel_tf_status *status)
+{
+	if (!session) {
+		noop_error("Invalid session\n");
+		return VACCEL_EINVAL;
+	}
+
+	if (!model) {
+		noop_error("Invalid TensorFlow model\n");
+		return VACCEL_EINVAL;
+	}
+
+	if (!vaccel_sess_has_resource(session, model->resource)) {
+		noop_error("Model is not registered with session\n");
+		return VACCEL_ENOENT;
+	}
+
+	if (status) {
+		status->message = strdup("Operation handled by noop plugin");
+		if (!status->message)
+			return VACCEL_ENOMEM;
+	}
+
+	return VACCEL_OK;
+}
+
+static int noop_tf_model_run(
+	struct vaccel_session *session,
+        const struct vaccel_tf_model *model, const struct vaccel_tf_buffer *run_options,
+        const struct vaccel_tf_node *in_nodes, const struct vaccel_tf_tensor *in, int nr_inputs,
+        const struct vaccel_tf_node *out_nodes, struct vaccel_tf_tensor *out, int nr_outputs,
+        struct vaccel_tf_status *status)
+{
+	if (!session) {
+		noop_error("Invalid session\n");
+		return VACCEL_EINVAL;
+	}
+
+	if (!model) {
+		noop_error("Invalid TensorFlow model\n");
+		return VACCEL_EINVAL;
+	}
+
+	if (run_options)
+		noop_info("Run options -> %p, %lu\n", run_options->data,
+			run_options->size);
+
+	noop_info("Number of inputs: %d\n", nr_inputs);
+	for (int i = 0; i < nr_inputs; ++i) {
+		noop_info("\tNode %d: %s:%ld\n", i, in_nodes[i].name,
+				in_nodes[i].id);
+		noop_info("\t#dims: %d -> {", in[i].nr_dims);
+		for (int j = 0; j < in[i].nr_dims; ++j)
+			printf("%ld%s", in[i].dims[j],
+				(j == in[i].nr_dims - 1) ? "}\n" : " ");
+
+		noop_info("\tData type: %d\n", in[i].data_type);
+		noop_info("\tData -> %p, %lu\n", in[i].data, in[i].size);
+	}
+
+	noop_info("Number of outputs: %d\n", nr_outputs);
+	for (int i = 0; i < nr_outputs; ++i) {
+		noop_info("\tNode %d: %s:%ld\n", i, out_nodes[i].name,
+				out_nodes[i].id);
+		out[i].nr_dims = 0;
+		out[i].dims = NULL;
+		out[i].data_type = 0;
+		out[i].data = NULL;
+		out[i].size = 0;
+	}
+
+	if (status) {
+		status->message = strdup("Operation handled by noop plugin");
+		if (!status->message)
+			return VACCEL_ENOMEM;
+	}
+
+	return VACCEL_OK;
+}
+
 struct vaccel_op ops[] = {
 	VACCEL_OP_INIT(ops[0], VACCEL_NO_OP, noop_noop),
 	VACCEL_OP_INIT(ops[1], VACCEL_BLAS_SGEMM, noop_sgemm),
@@ -91,6 +183,8 @@ struct vaccel_op ops[] = {
 	VACCEL_OP_INIT(ops[3], VACCEL_IMG_DETEC, noop_img_detect),
 	VACCEL_OP_INIT(ops[4], VACCEL_IMG_SEGME, noop_img_segme),
 	VACCEL_OP_INIT(ops[5], VACCEL_EXEC, noop_exec),
+	VACCEL_OP_INIT(ops[6], VACCEL_TF_MODEL_LOAD_GRAPH, noop_tf_model_load_graph),
+	VACCEL_OP_INIT(ops[7], VACCEL_TF_MODEL_RUN_GRAPH, noop_tf_model_run),
 };
 
 static int init(void)
