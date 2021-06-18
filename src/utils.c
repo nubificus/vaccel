3559efcbcf546e8d40f1e42ed3601228bb712528
Babis Chalios <mail@bchalios.io>
2021-06-18 09:34:04 +0200

    Add a util function to mmap a file
    We typically need to read files for TensorFlow models. Add a helper
    function that mmaps a file and returns a pointer to the memory.
    
    Signed-off-by: Babis Chalios <mail@bchalios.io>

--- src/utils.c
+++ src/utils.c
@@ -1,10 +1,16 @@
 #include "utils.h"
 #include "error.h"
+#include "log.h"
 
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <dirent.h>
 #include <stdbool.h>
 #include <unistd.h>
 #include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/mman.h>
 
 bool dir_exists(const char *path)
 {
@@ -24,4 +30,39 @@ int cleanup_rundir(const char *path)
 		return errno;
 
 	return VACCEL_OK;
+}
+
+int read_file(const char *path, void **data, size_t *size)
+{
+	int fd, ret;
+
+	fd = open(path, O_RDONLY);
+	if (fd == -1) {
+		vaccel_debug("Could not open file %s", path);
+		return errno;
+	}
+
+	/* Find the size of the file */
+	struct stat stat;
+	ret = fstat(fd, &stat);
+	if (ret < 0) {
+		vaccel_debug("Could not debug file %s", strerror(errno));
+		ret = errno;
+		goto close_file;
+	}
+
+	void *ptr = mmap(NULL, stat.st_size, PROT_READ|PROT_WRITE, MAP_PRIVATE,
+			fd, 0);
+	if (!ptr) {
+		vaccel_debug("Could not mmap file");
+		ret = VACCEL_ENOMEM;
+		goto close_file;
+	}
+
+	*data = ptr;
+	*size = stat.st_size;
+
+close_file:
+	close(fd);
+	return ret;
 }
--- src/utils.h
+++ src/utils.h
@@ -2,6 +2,7 @@
 #define __UTILS_H__
 
 #include <stdbool.h>
+#include <stddef.h>
 
 /* Check if a directory exists */
 bool dir_exists(const char *path);
@@ -10,5 +11,12 @@ bool dir_exists(const char *path);
  * return VACCEL_OK at success, or a proper error code otherwise
  */
 int cleanup_rundir(const char *path);
+
+/* Read a file into a buffer
+ *
+ * This will actually mmap the file with read and write
+ * access and it will reteurn the mapped memory and the
+ * size of the file */
+int read_file(const char *path, void **data, size_t *size);
 
 #endif /* __UTILS_H__ */
