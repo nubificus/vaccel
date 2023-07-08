1677c35b590b9356d93ab7498b653e369cf2a97d
Kostis Papazafeiropoulos <papazof@gmail.com>
2023-07-08 18:37:11 +0300

    meson: Build deb package with meson dist
    Signed-off-by: Kostis Papazafeiropoulos <papazof@gmail.com>
    Reviewed-by: Anastassios Nanos <ananos@nubificus.co.uk>
    Approved-by: Anastassios Nanos <ananos@nubificus.co.uk>
    PR: https://github.com/cloudkernels/vaccelrt/pull/106

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -1,4 +1,32 @@
 #!/bin/sh
 
-cd "$MESON_SOURCE_ROOT" || exit 1
-./scripts/generate-version.sh > "$MESON_DIST_ROOT"/.version
+# generate .version file
+cd ${MESON_SOURCE_ROOT} || exit 1
+VERSION=$(sh ./scripts/generate-version.sh)
+echo ${VERSION} > "${MESON_DIST_ROOT}/.version"
+
+# build .deb packages
+# (needs: meson dist --include-subprojects)
+cd ${MESON_DIST_ROOT}
+rm -f "../*${VERSION}.orig.tar.xz"
+rm -rf "${MESON_DIST_ROOT}/.git*"
+PKG_NAME=$1 && shift
+MESON_ARGS=""                                                                   
+c=$((0))                                                                        
+for v in $@                                                                     
+do                                                                              
+	if [ $((c % 2)) -eq 0 ]                                                     
+	then                                                                        
+		MESON_ARGS="${MESON_ARGS}-D$v="                                         
+	else                                                                        
+		MESON_ARGS="${MESON_ARGS}$v "                                           
+	fi                                                                          
+	c=$((c+1))                                                                  
+done
+USER="$(whoami)" \
+	DEBFULLNAME="Anastassios Nanos" DEBEMAIL="ananos@nubificus.co.uk" \
+	dh_make -s -y -c apache -p "${PKG_NAME}_${VERSION}" --createorig
+echo "override_dh_auto_configure:\n\tdh_auto_configure --buildsystem=meson -- ${MESON_ARGS}" \
+	>> debian/rules
+dpkg-buildpackage -us -uc
+rm -r obj-* debian
