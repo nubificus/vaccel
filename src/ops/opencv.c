0ae43bca2401ef0d53efc534e5e3c8d6f1a25fda
Maria Goutha <mgouth@nubificus.co.uk>
2023-10-25 09:33:27 +0300

    Create vaccel_opencv

--- src/ops/opencv.c
+++ src/ops/opencv.c
@@ -3,43 +3,255 @@
 #include "log.h"
 #include "vaccel_ops.h"
 #include "genop.h"
-//#include "opencv.h"
-
 #include "session.h"
-int vaccel_opencv(struct vaccel_session *sess, struct vaccel_arg *read,
-		int nr_read, struct vaccel_arg *write, int nr_write)
+#include "vaccel_prof.h"
+#include "opencv.h"
+
+typedef struct {
+    int x;
+    int y;
+} Point2i;
+
+typedef struct {
+	size_t size;
+    Point2i* data;
+} Point2iVector;
+
+typedef struct {
+	size_t size;
+    Point2iVector* data;
+} VectorOfPoint2iVector;
+
+typedef struct {
+    int x;
+    int y;
+	int z;
+	int w;
+} Point4i;
+
+typedef struct {
+	size_t size;
+    Point4i* data;
+} Point4iVector;
+
+typedef struct {
+	size_t size;
+    Point4iVector* data;
+} VectorOfPoint4iVector;
+
+typedef struct {
+    float x;
+    float y;
+} Point2f;
+
+typedef struct {
+	size_t size;
+    Point2f* data;
+} Point2fVector;
+
+typedef struct {
+	size_t size;
+    Point2fVector *data;
+} VectorOfPoint2fVector;
+
+typedef struct {
+    float x;
+    float y;
+	float z;
+} Point3f;
+
+typedef struct {
+	size_t size;
+    Point3f* data;
+} Point3fVector;
+
+typedef struct {
+	size_t size;
+    Point3fVector *data;
+} VectorOfPoint3fVector;
+
+typedef struct {
+    int width;
+    int height;
+} Size;
+
+typedef struct {
+    int rows;
+    int columns;
+	float* data;
+} Mat;
+
+typedef struct {
+	size_t size;
+    unsigned char* data;
+} UcharVector;
+
+typedef struct {
+	size_t size;
+    float* data;
+} FloatVector;
+
+typedef struct {
+    int type;
+    int maxCount;
+    double epsilon;
+} TermCriteria;
+
+struct vaccel_prof_region opencv_op_stats =
+	VACCEL_PROF_REGION_INIT("vaccel_opencv_op");
+
+int vaccel_opencv(enum vaccel_op_type op_type, struct vaccel_session *sess)
 {
+	int ret;
+
 	if (!sess)
 		return VACCEL_EINVAL;
 
-	vaccel_debug("session:%u Looking for plugin implementing the Optical Flow operation",
-			sess->session_id);
+	vaccel_debug("session:%u Looking for plugin implementing %s",
+			sess->session_id, vaccel_op_type_str(op_type));
+
+	vaccel_prof_region_start(&opencv_op_stats);
 
 	//Get implementation
-	int (*plugin_op)() = get_plugin_op(VACCEL_OPENCV, sess->hint);
+	int (*plugin_op)() = get_plugin_op(op_type, sess->hint);
 	if (!plugin_op)
 		return VACCEL_ENOTSUP;
+		goto out;
 
-	return plugin_op(sess, read, nr_read, write, nr_write);
+	ret = plugin_op(sess, op_type);
+
+out:
+	vaccel_prof_region_stop(&opencv_op_stats);
+	return ret;
 }
 
-int vaccel_opencv_unpack(struct vaccel_session *sess, struct vaccel_arg *read,
-		int nr_read, struct vaccel_arg *write, int nr_write)
+#define vaccel_opencv_op_no_text(op_type, sess) \
+		vaccel_opencv_op(op_type, sess)
+
+int vaccel_opencv_unpack(enum vaccel_op_type op_type,
+		struct vaccel_session *sess, struct vaccel_arg *read,
+		int nr_read, int nr_read_req, struct vaccel_arg *write,
+		int nr_write, int nr_write_req)
 {
 //TODO: sanity check args
-#if 0
-	if (nr_read != 2) {
-		vaccel_error("Wrong number of read arguments in FOO: %d",
-				nr_read);
+#if 1
+	if (nr_read != nr_read_req) {
+		vaccel_error("Wrong number of read arguments in %s: %d (expected %d)",
+				vaccel_op_type_str(op_type), nr_read,
+				nr_read_req);
 		return VACCEL_EINVAL;
 	}
 
-	if (nr_write != 1) {
-		vaccel_error("Wrong number of write arguments in FOO: %d",
-				nr_write);
+	if (nr_write != nr_write_req) {
+		vaccel_error("Wrong number of write arguments in %s: %d (expected %d)",
+				vaccel_op_type_str(op_type), nr_write,
+				nr_write_req);
 		return VACCEL_EINVAL;
 	}
 #endif
-	return vaccel_opencv(sess, read, nr_read, write, nr_write);
+
+	return vaccel_opencv(op_type,sess);
 }
 
+void vaccel_calcOpticalFlowPyrLK(struct vaccel_session *session, Mat prevFrame, Mat currentFrame,
+			Point2fVector prevPoints, Point2fVector *currentPoints, UcharVector *status,
+			FloatVector *err, Size winSize, int maxLevel, TermCriteria criteria)
+{
+	return vaccel_opencv_op(VACCEL_OPENCV_OPTFLOW, session);
+}
+
+
+bool vaccel_findChessboardCorners(struct vaccel_session *session, Mat image,
+							Size pattern_size, Point2fVector *corners)
+{
+	return vaccel_opencv_op(VACCEL_OPENCV_FIND_CHESSBOARD_CORNERS, session);
+}
+
+
+void vaccel_calibrateCamera(struct vaccel_session *session, VectorOfPoint3fVector *objpoints, 
+						VectorOfPoint2fVector *imgpoints, Size *imagesize,
+	 					Mat *cameraMatrix, Mat *distCoeffs, Mat *R, Mat *T)
+{
+	return vaccel_opencv_op(VACCEL_OPENCV_CALIBRATE_CAMERA, session);
+}
+
+
+cv::Mat vaccel_getOptimalNewCameraMatrix(struct vaccel_session *session, Mat cameraMatrix, Mat distCoeffs,
+                                       Size imgSize, double alpha)
+{
+	return vaccel_opencv_op(VACCEL_OPENCV_GET_OPTIMAL_NEW_CAMERA_MATRIX, session);
+}
+
+
+void  vaccel_stereoCalibrate(struct vaccel_session *session, VectorOfPoint3fVector *objpoints,
+									VectorOfPoint2fVector *imgpointsL, VectorOfPoint2fVector *imgpointsR,
+									Mat *new_mtxL, Mat *distL, Mat *new_mtxR, Mat *distR, Size *imagesizeR,
+									void *Rot, void *Trns, void *Emat, void *Fmat,
+									int flag, TermCriteria criteria)
+{
+	return vaccel_opencv_op(VACCEL_OPENCV_STEREO_CALIBRATE, session);
+}
+
+
+void vaccel_stereoRectify(struct vaccel_session *session, Mat *new_mtxL, Mat *distL, Mat *new_mtxR, Mat *distR,
+                    				Size *imagesizeR, Mat *Rot, Mat *Trns,
+									Mat *rect_l,  Mat *rect_r,  Mat *proj_mat_l,  Mat *proj_mat_r,  Mat *Q, int rec_flag)
+{
+	return vaccel_opencv_op(VACCEL_OPENCV_STEREO_RECTIFY, session);
+}
+
+
+void vaccel_remap(struct vaccel_session *session,
+				Mat *src, Mat *dst, Mat *map1, Mat *map2,
+				int interpolation, int border_mode, int border_value)
+{
+	return vaccel_opencv_op(VACCEL_OPENCV_REMAP, session);
+}
+
+
+void vaccel_inRange(struct vaccel_session *session, Mat depth_map, float lowerb, float upperb, Mat mask)
+{
+	return vaccel_opencv_op(VACCEL_OPENCV_IN_RANGE, session);
+}
+
+
+void vaccel_findContours(struct vaccel_session *session, Mat *mask, VectorOfPoint2iVector contours, VectorOfPoint4iVector hierarchy, int mode, int method)
+{
+	return vaccel_opencv_op(VACCEL_OPENCV_FIND_CONTOURS, session);
+}
+
+
+void vaccel_solve(struct vaccel_session *session, Mat *coeff, Mat *Z_mat, Mat *sol, int method)
+{
+	return vaccel_opencv_op(VACCEL_OPENCV_SOLVE, session);
+}
+
+
+cv::Ptr<cv::StereoBM> vaccel_stereoBM_create_compute(struct vaccel_session *session, int numDisparities, int blockSize, 
+													int preFilterType, int preFilterSize, int preFilterCap,
+													int textureThreshold,int uniquenessRatio, int speckleRange,
+													int speckleWindowSize,int disp12MaxDiff, int minDisparity,
+													Mat Left_nice, Mat Right_nice, Mat disp)
+{
+	return vaccel_opencv_op(VACCEL_OPENCV_BM_CREATE_COMPUTE, session);
+}
+
+
+cv::Ptr<cv::StereoSGBM> vaccel_stereoSGBM_create_compute(struct vaccel_session *session, int minDisparity, int numDisparities,
+													int blockSize, int disp12MaxDiff, int uniquenessRatio, int speckleWindowSize,
+													int speckleRange, Mat Left_nice, Mat Right_nice, Mat disp)
+{
+	return vaccel_opencv_op(VACCEL_OPENCV_SGBM_CREATE_COMPUTE, session);
+}
+
+
+__attribute__((constructor))
+static void vaccel_tf_ops_init(void)
+{
+}
+
+__attribute__((destructor))
+static void vaccel_tf_ops_fini(void)
+{
+	vaccel_prof_region_print(&opencv_op_stats);
+}
--- src/ops/opencv.h
+++ src/ops/opencv.h
@@ -6,7 +6,7 @@
 //struct vaccel_session;
 //struct vaccel_arg;
 
-//int vaccel_optflow(struct vaccel_session *sess, struct vaccel_arg *read,
+//int vaccel_opencv(struct vaccel_session *sess, struct vaccel_arg *read,
 		//int nr_read, struct vaccel_arg *write, int nr_write);
 
 
@@ -14,14 +14,52 @@
 #include <stdint.h>
 #include <stddef.h>
 
-//#include "include/ops/optflow.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+enum vaccel_op_type {
+	VACCEL_NO_OP=0,
+	VACCEL_OPENCV_OPTFLOW,							/* 1 */
+	VACCEL_OPENCV_FIND_CHESSBOARD_CORNERS,			/* 2 */
+	VACCEL_OPENCV_CORNER_SUB_PIX,					/* 3 */
+	VACCEL_OPENCV_CALIBRATE_CAMERA,					/* 4 */
+	VACCEL_OPENCV_GET_OPTIMAL_NEW_CAMERA_MATRIX,	/* 5 */
+	VACCEL_OPENCV_STEREO_CALIBRATE,					/* 6 */
+	VACCEL_OPENCV_STEREO_RECTIFY,					/* 7 */
+	VACCEL_OPENCV_INIT_UNDISTORT_RECTIFY_MAP,		/* 8 */
+	VACCEL_OPENCV_REMAP,							/* 9 */
+	VACCEL_OPENCV_IN_RANGE,							/* 10 */
+	VACCEL_OPENCV_FIND_CONTOURS,					/* 11 */
+	VACCEL_OPENCV_SOLVE,							/* 12 */
+	VACCEL_OPENCV_BM_CREATE_COMPUTE,				/* 13 */
+	VACCEL_OPENCV_SGBM_CREATE_COMPUTE				/* 14 */
+};
 
-int vaccel_opencv_unpack(struct vaccel_session *sess, struct vaccel_arg *read,
-		int nr_read, struct vaccel_arg *write, int nr_write);
+static const char *vaccel_op_name[] = {
+	"OpenCV optflow",
+	"OpenCV find chessboard corners",
+	"OpenCV corner sub pix",
+	"OpenCV calibrate camera",
+	"OpenCV get optimal new camera matrix",
+	"OpenCV stereo calibrate",
+	"OpenCV stereo rectify",
+	"OpenCV init undistort rectify map",
+	"OpenCV remap",
+	"OpenCV in range",
+	"OpenCV find contours",
+	"OpenCV solve",
+	"OpenCV stereoBM",
+	"OpenCV stereoSGBM"
+};
+
+static inline const char *vaccel_op_type_str(enum vaccel_op_type op_type)
+{
+	return vaccel_op_name[op_type];
+}
+
+// int vaccel_opencv_unpack(struct vaccel_session *sess, struct vaccel_arg *read,
+// 		int nr_read, struct vaccel_arg *write, int nr_write);
 
 #ifdef __cplusplus
 }
