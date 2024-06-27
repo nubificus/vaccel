d804cd089ae3b524ad7919387adbde052f6acd3e
Kostis Papazafeiropoulos <papazof@gmail.com>
2024-06-27 12:14:59 +0000

    build(meson): Improve scripts according to linter suggestions
    PR: https://github.com/cloudkernels/vaccelrt/pull/111
    Signed-off-by: Kostis Papazafeiropoulos <papazof@gmail.com>
    Reviewed-by: Anastassios Nanos <ananos@nubificus.co.uk>
    Approved-by: Anastassios Nanos <ananos@nubificus.co.uk>

--- scripts/generate-version.sh
+++ scripts/generate-version.sh
@@ -27,15 +27,15 @@ then
 fi
 
 DIRTY=$(git diff --quiet || echo '-dirty')
-for i in $@
+for i in "$@"
 do
-	if [ $i = "--no-dirty" ]
+	if [ "$i" = "--no-dirty" ]
 	then
 		DIRTY=""
 		break
 	fi
 done
 
-VERSION=$(echo ${VERSION}${DIRTY} | sed -e 's/-g/-/' | cut -c 2-)
+VERSION=$(echo "${VERSION}${DIRTY}" | sed -e 's/-g/-/' | cut -c 2-)
 
-echo ${VERSION}
+echo "${VERSION}"
