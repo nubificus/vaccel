4cc3fcee71cf76a8b529aa9a0d2735f81205d34d
Kostis Papazafeiropoulos <papazof@gmail.com>
2024-08-02 18:31:29 +0000

    build(meson): Improve deb changelog
    - Add all commits since the beginning of the repo
    - Remove placeholder lines
    - Exclude github-actions' bot commits
    
    PR: https://github.com/nubificus/vaccel/pull/132
    Signed-off-by: Kostis Papazafeiropoulos <papazof@gmail.com>
    Reviewed-by: Ilias Lagomatis <ilago@nubificus.co.uk>
    Approved-by: Ilias Lagomatis <ilago@nubificus.co.uk>

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -62,22 +62,28 @@ printf "%s\n\t%s" "override_dh_auto_configure:" \
 	>>debian/rules
 
 # debian/copyright
-sed -i "s/Upstream-Contact.*/Upstream-Contact: ${DEBFULLNAME} <${DEBEMAIL}>/g" debian/copyright
-sed -i "s/Source.*/Source: https:\/\/github.com\/nubificus\/vaccel/g" debian/copyright
-sed -i "s/Upstream-Contact.*/Upstream-Contact: ${DEBFULLNAME} <${DEBEMAIL}>/g" debian/copyright
-sed -i "s/Copyright.*/Copyright: 2020-$(date +"%Y") Nubificus LTD <info@nubificus.co.uk>/g" debian/copyright
+sed -i "s/Upstream-Contact.*/Upstream-Contact: ${DEBFULLNAME} <${DEBEMAIL}>/g" \
+	debian/copyright
+sed -i "s/Source.*/Source: https:\/\/github.com\/nubificus\/vaccel/g" \
+	debian/copyright
+sed -i "s/Upstream-Contact.*/Upstream-Contact: ${DEBFULLNAME} <${DEBEMAIL}>/g" \
+	debian/copyright
+sed -i "s/Copyright.*/Copyright: 2020-$(date +"%Y") Nubificus LTD <info@nubificus.co.uk>/g" \
+	debian/copyright
 sed -i "/<years>/d" debian/copyright
 sed -i "/^#.*/d" debian/copyright
 
 # debian/control
 sed -i "s/Homepage.*/Homepage: https:\/\/vaccel.org/g" debian/control
 sed -i "s/Section.*/Section: libs/g" debian/control
-sed -i "s/Description.*/Description: Hardware Acceleration for Serverless Computing/g" debian/control
+sed -i "s/Description.*/Description: Hardware Acceleration for Serverless Computing/g" \
+	debian/control
 sed -i "/^#.*/d" debian/control
 sed -i "/<insert/d" debian/control
 
 # debian/changelog
 echo "Generating changelog..."
+sed -i "2d;3d" debian/changelog
 cp -r "${MESON_SOURCE_ROOT}"/.git* ./
 TAG=$(git describe --abbrev=0 --tags --match "v[0-9]*" 2>/dev/null)
 TAG_DIFF=$(git describe --abbrev=8 --tags --match "v[0-9]*" 2>/dev/null)
@@ -85,16 +91,29 @@ ORIG_COMMIT=$(git rev-parse HEAD)
 for t in $(git tag --sort=v:refname | grep "v[0-9]*"); do
 	CUR_VERSION="$(echo "$t" | cut -c 2-)-1"
 	git checkout "$t" 1>/dev/null 2>&1
-	PREV_TAG=$(git describe --abbrev=0 --tags --match "v[0-9]*" --exclude="$t" 2>/dev/null) || continue
-	gbp dch --since="${PREV_TAG}" --ignore-branch --release --spawn-editor=never --new-version="${CUR_VERSION}" --dch-opt="-b" --dch-opt="--check-dirname-level=0" --dch-opt="-p" 1>/dev/null 2>&1
+	PREV_TAG=$(git describe --abbrev=0 --tags --match "v[0-9]*" \
+		--exclude="$t" 2>/dev/null) ||
+		PREV_TAG=$(git rev-list --max-parents=0 HEAD | tail -n 1)
+	gbp dch --since="${PREV_TAG}" --ignore-branch --release \
+		--spawn-editor=never --new-version="${CUR_VERSION}" \
+		--dch-opt="-b" --dch-opt="--check-dirname-level=0" \
+		--dch-opt="-p" \
+		--git-log="--invert-grep --grep=Signed-off-by:.*github-actions" \
+		1>/dev/null 2>&1
 done
 if [ "${TAG}" != "${TAG_DIFF}" ]; then
 	git checkout "${ORIG_COMMIT}" 1>/dev/null 2>&1
-	PREV_TAG=$(git describe --abbrev=0 --tags --match "v[0-9]*" 2>/dev/null)
+	PREV_TAG=$(git describe --abbrev=0 --tags --match "v[0-9]*" \
+		2>/dev/null) ||
+		PREV_TAG=$(git rev-list --max-parents=0 HEAD | tail -n 1)
 	CUR_VERSION="${VERSION}-1"
-	gbp dch --since="${PREV_TAG}" --ignore-branch --release --spawn-editor=never --new-version="${CUR_VERSION}" --dch-opt="-b" --dch-opt="--check-dirname-level=0" --dch-opt="-p" 1>/dev/null 2>&1
+	gbp dch --since="${PREV_TAG}" --ignore-branch --release \
+		--spawn-editor=never --new-version="${CUR_VERSION}" \
+		--dch-opt="-b" --dch-opt="--check-dirname-level=0" \
+		--dch-opt="-p" \
+		--git-log="--invert-grep --grep=Signed-off-by:.*github-actions" \
+		1>/dev/null 2>&1
 fi
-head -n -6 debian/changelog >debian/changelog_ && rm debian/changelog && mv debian/changelog_ debian/changelog
 rm -rf "${MESON_DIST_ROOT}"/.git*
 
 rm -rf debian/*.ex debian/*.EX debian/*.docs debian/README*
