f13f1d017761258cbb63c10df7273ad067d16504
Ilias Lagomatis <ilago@nubificus.co.uk>
2024-11-29 12:07:14 +0200

    refactor(torch-inference): Perform actual classification
    Also uploaded a mobilenet v2 pytorch model in
    S3 to use: `https://s3.nbfc.io/torch/mobilenet.pt`
    and ImageNet labels in `s3.nbfc.io/imagenet/label.txt`
    
    PR: https://github.com/nubificus/vaccel/pull/148
    Signed-off-by: Ilias Lagomatis <ilago@nubificus.co.uk>
    Reviewed-by: Kostis Papazafeiropoulos <papazof@gmail.com>
    Approved-by: Kostis Papazafeiropoulos <papazof@gmail.com>

--- examples/torch_inference.cpp
+++ examples/torch_inference.cpp
@@ -5,6 +5,7 @@
 #include <cstdlib>
 #include <cstring>
 #include <fcntl.h>
+#include <fstream>
 #include <iostream>
 #include <random>
 #include <session.h>
@@ -20,9 +21,76 @@
 
 #include <vaccel.h>
 
-auto random_input_generator(int min_value = 1, int max_value = 100,
-			    size_t vector_size = 150528,
-			    bool is_print = true) -> std::vector<float>
+auto load_labels(const std::string &file_name,
+		 std::vector<std::string> &labels) -> bool
+{
+	std::ifstream ifs(file_name);
+	if (!ifs)
+		return false;
+
+	std::string line;
+	while (std::getline(ifs, line))
+		labels.push_back(line);
+
+	return true;
+}
+
+auto preprocess(const unsigned char *image_data, int width, int height,
+		int channels, float *output_buffer) -> int
+{
+	if (channels != 3) {
+		std::cerr << "Error: Only 3-channel RGB images are supported\n";
+		return VACCEL_EINVAL;
+	}
+
+	/* Target dimensions for resizing */
+	const int target_width = 224;
+	const int target_height = 224;
+
+	/* Mean and standard deviation for normalization */
+	const float mean[] = { 0.485F, 0.456F, 0.406F };
+	const float std[] = { 0.229F, 0.224F, 0.225F };
+
+	/* Allocate intermediate buffer for resized image */
+	std::vector<unsigned char> resized_image(
+		(size_t)(target_width * target_height * 3));
+
+	/* Resize manually (nearest neighbor interpolation) */
+	for (int y = 0; y < target_height; ++y) {
+		for (int x = 0; x < target_width; ++x) {
+			int src_x = x * width / target_width;
+			int src_y = y * height / target_height;
+			for (int c = 0; c < 3; ++c) {
+				resized_image[(y * target_width + x) * 3 + c] =
+					image_data[(src_y * width + src_x) * 3 +
+						   c];
+			}
+		}
+	}
+
+	/* Normalize and reorder channels to [C, H, W] */
+	for (int c = 0; c < 3; ++c) {
+		for (int y = 0; y < target_height; ++y) {
+			for (int x = 0; x < target_width; ++x) {
+				unsigned char pixel_value =
+					resized_image[(y * target_width + x) * 3 +
+						      c];
+				float normalized_value =
+					((float)pixel_value / 255.0F -
+					 mean[c]) /
+					std[c];
+				output_buffer[c * target_width * target_height +
+					      y * target_width + x] =
+					normalized_value;
+			}
+		}
+	}
+	return VACCEL_OK;
+}
+
+auto generate_random_input(int min_value = 1, int max_value = 100,
+			   size_t vector_size = 150528,
+			   bool is_print = true) -> std::vector<float>
 {
 	// Create a random number generator engine
 	std::random_device rd;
@@ -48,29 +116,48 @@ auto random_input_generator(int min_value = 1, int max_value = 100,
 	return res_data;
 }
 
+const int target_width = 224;
+const int target_height = 224;
+const int target_channels = 3;
+
+#ifdef USE_STB_IMAGE
+const int nr_req_args = 4;
+const std::string usage_args = "image-path model-path labels-path";
+#else
+const int nr_req_args = 3;
+const std::string usage_args = "image-path model-path";
+#endif
+
 auto main(int argc, char **argv) -> int
 {
+	if (argc < nr_req_args) {
+		std::cerr << "Usage: " << argv[0] << " " << usage_args << '\n';
+		return VACCEL_EINVAL;
+	}
+
 	struct vaccel_session sess;
 	struct vaccel_resource model;
 	struct vaccel_torch_buffer run_options;
 	int ret;
-	char *model_path = argv[2];
-	int64_t dims[] = { 1, 224, 224, 3 };
+	int64_t dims[] = { 1, 3, 224, 224 };
 	struct vaccel_torch_tensor *in;
 	struct vaccel_torch_tensor *out;
 	std::vector<float> res_data;
+#ifdef USE_STB_IMAGE
+	float *preprocessed_data = nullptr;
+	float max;
+	unsigned int max_idx;
+	float *response_ptr;
+	std::vector<std::string> labels;
+#endif
 
-	if (argc != 3) {
-		fprintf(stderr, "Usage: %s image filename, model path\n",
-			argv[0]);
-		exit(1);
-	}
-
-	ret = vaccel_resource_init(&model, model_path, VACCEL_RESOURCE_MODEL);
+	ret = vaccel_resource_init(&model, argv[2], VACCEL_RESOURCE_MODEL);
 	if (ret != 0) {
 		fprintf(stderr, "Could not create model resource\n");
 		return ret;
 	}
+
+	printf("Initialized vAccel resource %" PRId64 "\n", model.id);
 
 	ret = vaccel_session_init(&sess, 0);
 	if (ret != VACCEL_OK) {
@@ -80,15 +167,24 @@ auto main(int argc, char **argv) -> int
 
 	printf("Initialized vAccel session %" PRId64 "\n", sess.id);
 
-	/* Take vaccel_session and vaccel_resource as inputs, 
-	 * register a resource with a session */
 	ret = vaccel_resource_register(&model, &sess);
 	if (ret != VACCEL_OK) {
 		fprintf(stderr, "Could not register model with session\n");
 		goto close_session;
 	}
 
+	in = vaccel_torch_tensor_new(4, dims, VACCEL_TORCH_FLOAT);
+	if (in == nullptr) {
+		fprintf(stderr, "Could not allocate memory\n");
+		goto unregister_session;
+	}
+
 #ifdef USE_STB_IMAGE
+	if (!load_labels(argv[3], labels)) {
+		fprintf(stderr, "Could not load labels from %s", argv[3]);
+		return VACCEL_ENOENT;
+	}
+
 	int width;
 	int height;
 	int channels;
@@ -97,31 +193,44 @@ auto main(int argc, char **argv) -> int
 	img_data = stbi_load(argv[1], &width, &height, &channels, 0);
 	if (img_data == nullptr) {
 		std::cerr << "Failed to load image\n";
-		return -1;
-	}
-
-	res_data.reserve((size_t)width * (size_t)height * (size_t)channels);
-
-	for (int i = 0; i < width * height * channels; ++i) {
-		res_data.push_back((float)img_data[i] /
-				   255.0F); // Normalize pixel value
-	}
-	stbi_image_free(img_data);
-#else
-	res_data = random_input_generator();
-	res_data.resize((size_t)(224 * 224 * 3));
-#endif
-
-	in = vaccel_torch_tensor_new(4, dims, VACCEL_TORCH_FLOAT);
-	if (in == nullptr) {
-		fprintf(stderr, "Could not allocate memory\n");
+		ret = VACCEL_EINVAL;
 		goto unregister_session;
 	}
 
+	preprocessed_data = new float[(size_t)(target_width * target_height *
+					       target_channels)];
+	if (preprocessed_data == nullptr) {
+		fprintf(stderr, "Could not allocate memory to process data");
+		stbi_image_free(img_data);
+		ret = VACCEL_ENOMEM;
+		goto unregister_session;
+	}
+
+	memset(preprocessed_data, 0,
+	       (size_t)(target_width * target_height * target_channels) *
+		       sizeof(float));
+
+	ret = preprocess(img_data, width, height, channels, preprocessed_data);
+	if (ret != 0) {
+		fprintf(stderr, "Could not preprocess image data");
+		stbi_image_free(img_data);
+		goto unregister_session;
+	}
+
+	stbi_image_free(img_data);
+
+	in->data = (void *)preprocessed_data;
+	in->size = (size_t)(target_width * target_height * target_channels) *
+		   sizeof(float);
+#else
+	res_data = generate_random_input();
+	res_data.resize((size_t)(224 * 224 * 3));
+
 	in->data = res_data.data();
 	in->size = res_data.size() * sizeof(float);
+#endif
 
-	run_options.data = strdup("resnet");
+	run_options.data = strdup("mobilenet");
 	run_options.size = strlen(run_options.data) + 1;
 
 	/* Output tensor */
@@ -144,15 +253,22 @@ auto main(int argc, char **argv) -> int
 	printf("Result Tensor :\n");
 	printf("Output tensor => type:%u nr_dims:%u\n", out->data_type,
 	       out->nr_dims);
-	printf("data: %f\n", *(float *)out->data);
 	printf("size: %zu\n", out->size);
-	/*
-	   const char* classes[10] = { "plane", "car", "bird", 
-	   "cat", "deer", "dog", 
-	   "frog", "horse", "ship", "truck" };
 
-	   printf("Pred: %s\n", classes[int(offsets)])
-	   */
+#ifdef USE_STB_IMAGE
+	response_ptr = reinterpret_cast<float *>(out->data);
+	max = response_ptr[0];
+	max_idx = 0;
+
+	for (auto i = 1; i != 1000; ++i) {
+		if (response_ptr[i] > max) {
+			max = response_ptr[i];
+			max_idx = i;
+		}
+	}
+
+	std::cout << "Prediction: " << labels[max_idx] << '\n';
+#endif
 
 	if (vaccel_torch_tensor_destroy(out) != VACCEL_OK)
 		fprintf(stderr, "Could not destroy out tensor\n");
@@ -161,6 +277,9 @@ free_tensor:
 	if (vaccel_torch_tensor_destroy(in) != VACCEL_OK)
 		fprintf(stderr, "Could not destroy in tensor\n");
 unregister_session:
+#ifdef USE_STB_IMAGE
+	delete[] preprocessed_data;
+#endif
 	if (vaccel_resource_unregister(&model, &sess) != VACCEL_OK)
 		fprintf(stderr, "Could not unregister model with session\n");
 close_session:
