3b12b01343316512e928a8d1a1bb8d1302b6310a
Kostis Papazafeiropoulos <papazof@gmail.com>
2025-06-13 20:10:14 +0000

    refactor: Improve handling of buffer blobs
    Improve handling of buffer blobs in resource/blob:
    - Improve `vaccel_blob_init_from_buf()` and `vaccel_blob_release()` for
      different blob types, taking into account their use in bindings
    - Follow the data owing scheme of the provided blobs when creating a
      resource from blobs
    - Improve/cleanup `blob.c|h` docstrings
    - Use `_MAX` for blob validation instead of `_NONE`
    
    PR: https://github.com/nubificus/vaccel/pull/160
    Signed-off-by: Kostis Papazafeiropoulos <papazof@gmail.com>
    Reviewed-by: Kostis Papazafeiropoulos <papazof@gmail.com>
    Approved-by: Kostis Papazafeiropoulos <papazof@gmail.com>

--- src/include/vaccel/blob.h
+++ src/include/vaccel/blob.h
@@ -2,11 +2,10 @@
 
 #pragma once
 
+#include "utils/enum.h"
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
-#include "utils/enum.h"
-#include "utils/path.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -16,9 +15,8 @@ extern "C" {
  * vaccel_blob_type_to_base_str() */
 #define _ENUM_PREFIX VACCEL_BLOB
 #define VACCEL_BLOB_TYPE_ENUM_LIST(VACCEL_ENUM_ITEM) \
-	VACCEL_ENUM_ITEM(NONE, 0, _ENUM_PREFIX)      \
-	VACCEL_ENUM_ITEM(FILE, _ENUM_PREFIX)         \
-	VACCEL_ENUM_ITEM(BUF, _ENUM_PREFIX)          \
+	VACCEL_ENUM_ITEM(FILE, 0, _ENUM_PREFIX)      \
+	VACCEL_ENUM_ITEM(BUFFER, _ENUM_PREFIX)       \
 	VACCEL_ENUM_ITEM(MAPPED, _ENUM_PREFIX)
 
 VACCEL_ENUM_DEF_WITH_STR_FUNCS(vaccel_blob_type, _ENUM_PREFIX,
@@ -34,36 +32,57 @@ struct vaccel_blob {
 	/* name of the blob */
 	char *name;
 
-	/* Path to blob file */
+	/* path to the blob file; only set for non-buffer blobs */
 	char *path;
 
-	/* Do we own the file? */
+	/* true if the struct owns the blob file */
 	bool path_owned;
 
-	/* Pointer to the contents of the blob in case we hold them
-	 * in a buffer */
-	bool data_owned;
+	/* data content of the blob; can be `NULL` if a blob file has not been
+	 * read */
 	uint8_t *data;
+
+	/* true if the struct owns the data */
+	bool data_owned;
+
+	/* data size of the blob; can be `0` if a blob file has not been
+	 * read */
 	size_t size;
 };
 
-typedef struct vaccel_blob vaccel_blob;
-
+/* Persist a blob in the filesystem */
 int vaccel_blob_persist(struct vaccel_blob *blob, const char *dir,
 			const char *filename, bool randomize);
+
+/* Initialize a blob from an existing file in the filesystem */
 int vaccel_blob_init(struct vaccel_blob *blob, const char *path);
+
+/* Initialize a blob from in-memory data */
 int vaccel_blob_init_from_buf(struct vaccel_blob *blob, const uint8_t *buf,
 			      size_t size, bool own, const char *filename,
 			      const char *dir, bool randomize);
+
+/* Release blob resources */
 int vaccel_blob_release(struct vaccel_blob *blob);
+
+/* Allocate and initialize a blob from an existing file in the filesystem */
 int vaccel_blob_new(struct vaccel_blob **blob, const char *path);
+
+/* Allocate and initialize a blob from in-memory data */
 int vaccel_blob_from_buf(struct vaccel_blob **blob, const uint8_t *buf,
 			 size_t size, bool own, const char *filename,
 			 const char *dir, bool randomize);
+
+/* Release blob resources and free object */
 int vaccel_blob_delete(struct vaccel_blob *blob);
-bool vaccel_blob_initialized(struct vaccel_blob *blob);
+
+/* Read the blob in memory */
 int vaccel_blob_read(struct vaccel_blob *blob);
+
+/* Get the data of the blob */
 uint8_t *vaccel_blob_data(struct vaccel_blob *blob, size_t *size);
+
+/* Get the path of the blob */
 const char *vaccel_blob_path(struct vaccel_blob *blob);
 
 #ifdef __cplusplus
