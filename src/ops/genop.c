d4c7901d57e4a40b64cf165f8948124a8dfd7c35
Kostis Papazafeiropoulos <papazof@gmail.com>
2025-07-16 20:55:22 +0000

    refactor(ops/genop): Cleanup callback array
    Explicitly define operation enum <> callback mapping for genop unpack
    functions instead of trying to guess the correct enum item. Leave
    unimplemented operations undefined (`NULL`), so `vaccel_genop` can
    properly fail if an operation type is not implemented.
    
    PR: https://github.com/nubificus/vaccel/pull/184
    Signed-off-by: Kostis Papazafeiropoulos <papazof@gmail.com>
    Reviewed-by: Anastassios Nanos <ananos@nubificus.co.uk>
    Approved-by: Anastassios Nanos <ananos@nubificus.co.uk>

--- src/ops/genop.c
+++ src/ops/genop.c
@@ -18,46 +18,43 @@ typedef int (*unpack_func_t)(struct vaccel_session *sess,
 			     struct vaccel_arg *read, int nr_read,
 			     struct vaccel_arg *write, int nr_write);
 
-unpack_func_t callbacks[VACCEL_OP_MAX] = {
-	vaccel_noop_unpack, /* 0 */
-	vaccel_sgemm_unpack, /* 1 */
-	vaccel_image_classification_unpack, /* 2 */
-	vaccel_image_detection_unpack, /* 3 */
-	vaccel_image_segmentation_unpack, /* 4 */
-	vaccel_image_pose_unpack, /* 5 */
-	vaccel_image_depth_unpack, /* 6 */
-	vaccel_exec_unpack, /* 7 */
-	vaccel_noop_unpack, /* 8 */
-	vaccel_noop_unpack, /* 9 */
-	vaccel_noop_unpack, /* 10 */
-	vaccel_noop_unpack, /* 11 */
-	vaccel_noop_unpack, /* 12 */
-	vaccel_noop_unpack, /* 13 */
-	vaccel_noop_unpack, /* 14 */
-	vaccel_minmax_unpack, /* 15 */
-	vaccel_fpga_arraycopy_unpack, /* 16 */
-	vaccel_fpga_mmult_unpack, /* 17 */
-	vaccel_fpga_parallel_unpack, /* 18 */
-	vaccel_fpga_vadd_unpack, /* 19 */
-	vaccel_exec_with_res_unpack, /* 20 */
-	vaccel_noop_unpack, /* 21 */
-	vaccel_noop_unpack, /* 22 */
-	vaccel_opencv_unpack, /* 23 */
+static unpack_func_t callbacks[VACCEL_OP_MAX] = {
+	[VACCEL_OP_NOOP] = vaccel_noop_unpack,
+	[VACCEL_OP_BLAS_SGEMM] = vaccel_sgemm_unpack,
+	[VACCEL_OP_IMAGE_CLASSIFY] = vaccel_image_classification_unpack,
+	[VACCEL_OP_IMAGE_DETECT] = vaccel_image_detection_unpack,
+	[VACCEL_OP_IMAGE_SEGMENT] = vaccel_image_segmentation_unpack,
+	[VACCEL_OP_IMAGE_POSE] = vaccel_image_pose_unpack,
+	[VACCEL_OP_IMAGE_DEPTH] = vaccel_image_depth_unpack,
+	[VACCEL_OP_EXEC] = vaccel_exec_unpack,
+	[VACCEL_OP_MINMAX] = vaccel_minmax_unpack,
+	[VACCEL_OP_FPGA_ARRAYCOPY] = vaccel_fpga_arraycopy_unpack,
+	[VACCEL_OP_FPGA_MMULT] = vaccel_fpga_mmult_unpack,
+	[VACCEL_OP_FPGA_PARALLEL] = vaccel_fpga_parallel_unpack,
+	[VACCEL_OP_FPGA_VECTORADD] = vaccel_fpga_vadd_unpack,
+	[VACCEL_OP_EXEC_WITH_RESOURCE] = vaccel_exec_with_res_unpack,
+	[VACCEL_OP_OPENCV] = vaccel_opencv_unpack,
 };
 
 int vaccel_genop(struct vaccel_session *sess, struct vaccel_arg *read,
 		 int nr_read, struct vaccel_arg *write, int nr_write)
 {
-	if (!nr_read) {
-		vaccel_error("Calling genop without op type");
+	if (nr_read < 1) {
+		vaccel_error("Missing operation type");
 		return VACCEL_EINVAL;
 	}
 
-	vaccel_op_type_t op = *(vaccel_op_type_t *)read[0].buf;
-	if (op >= VACCEL_OP_MAX) {
-		vaccel_error("Invalid operation type: %u", op);
+	vaccel_op_type_t *op = (vaccel_op_type_t *)read[0].buf;
+	if (!op || *op >= VACCEL_OP_MAX) {
+		vaccel_error("Invalid operation type");
 		return VACCEL_EINVAL;
 	}
 
-	return callbacks[op](sess, &read[1], nr_read - 1, write, nr_write);
+	if (!callbacks[*op]) {
+		vaccel_error("Operation not implemented for %s",
+			     vaccel_op_type_to_str(*op));
+		return VACCEL_ENOTSUP;
+	}
+
+	return callbacks[*op](sess, &read[1], nr_read - 1, write, nr_write);
 }
