2fd2c51985ae8d6436ef8cbf0e137f33da577f70
Kostis Papazafeiropoulos <papazof@gmail.com>
2025-11-06 05:17:01 +0000

    refactor(error): Move all error values into a single enum
    Move all errors specified in separate `#define`s into a single enum
    
    PR: https://github.com/nubificus/vaccel/pull/200
    Signed-off-by: Kostis Papazafeiropoulos <papazof@gmail.com>
    Reviewed-by: Anastassios Nanos <ananos@nubificus.co.uk>
    Approved-by: Anastassios Nanos <ananos@nubificus.co.uk>

--- src/include/vaccel/error.h
+++ src/include/vaccel/error.h
@@ -8,32 +8,33 @@
 
 /* vaccel return codes */
 enum {
-	VACCEL_OK = 0 /* All Good :D */
+	VACCEL_OK = 0, /* All Good :D */
+	VACCEL_EINVAL = EINVAL, /* Invalid argument */
+	VACCEL_ENOMEM = ENOMEM, /* Out of memory */
+	VACCEL_ENOTSUP = ENOTSUP, /* Operation not supported */
+	VACCEL_EINPROGRESS = EINPROGRESS, /* Operation now in progress */
+	VACCEL_EBUSY = EBUSY, /* Device or resource busy */
+	VACCEL_EEXIST = EEXIST, /* File exists */
+	VACCEL_ENOENT = ENOENT, /* No such file or directory */
+	VACCEL_ELIBBAD = ELIBBAD, /* Accessing a corrupted shared library */
+	VACCEL_ENODEV = ENODEV, /* No such device */
+	VACCEL_EIO = EIO, /* I/O error */
+	VACCEL_ESESS = ECONNRESET, /* Connection reset by peer */
+	VACCEL_EBACKEND = EPROTO, /* Protocol error */
+	VACCEL_ENOEXEC = ENOEXEC, /* Exec format error */
+	VACCEL_ENAMETOOLONG = ENAMETOOLONG, /* File name too long */
+	VACCEL_EUSERS = EUSERS, /* Too many users */
+	VACCEL_EPERM = EPERM, /* Operation not permitted */
+	VACCEL_ELOOP = ELOOP, /* Too many symbolic links encountered */
+	VACCEL_EMLINK = EMLINK, /* Too many links */
+	VACCEL_ENOSPC = ENOSPC, /* No space left on device */
+	VACCEL_ENOTDIR = ENOTDIR, /* Not a directory */
+	VACCEL_EROFS = EROFS, /* Read-only file system */
+	VACCEL_EACCES = EACCES, /* Permission denied */
+	VACCEL_EBADF = EBADF, /* Bad file number */
+	VACCEL_EREMOTEIO = EREMOTEIO, /* Remote I/O error */
+	VACCEL_EFAULT = EFAULT, /* Bad address */
+	VACCEL_ERANGE = ERANGE, /* Out of range */
+	VACCEL_EAGAIN = EAGAIN, /* Resource temporarily unavailable */
+	VACCEL_ENOSYS = ENOSYS, /* Function not implemented */
 };
-#define VACCEL_EINVAL EINVAL /* Invalid argument */
-#define VACCEL_ENOMEM ENOMEM /* Out of memory */
-#define VACCEL_ENOTSUP ENOTSUP /* Operation not supported */
-#define VACCEL_EINPROGRESS EINPROGRESS /* Operation now in progress */
-#define VACCEL_EBUSY EBUSY /* Device or resource busy */
-#define VACCEL_EEXIST EEXIST /* File exists */
-#define VACCEL_ENOENT ENOENT /* No such file or directory */
-#define VACCEL_ELIBBAD ELIBBAD /* Accessing a corrupted shared library */
-#define VACCEL_ENODEV ENODEV /* No such device */
-#define VACCEL_EIO EIO /* I/O error */
-#define VACCEL_ESESS ECONNRESET /* Connection reset by peer */
-#define VACCEL_EBACKEND EPROTO /* Protocol error */
-#define VACCEL_ENOEXEC ENOEXEC /* Exec format error */
-#define VACCEL_ENAMETOOLONG ENAMETOOLONG /* File name too long */
-#define VACCEL_EUSERS EUSERS /* Too many users */
-#define VACCEL_EPERM EPERM /* Operation not permitted */
-#define VACCEL_ELOOP ELOOP /* Too many symbolic links encountered */
-#define VACCEL_EMLINK EMLINK /* Too many links */
-#define VACCEL_ENOSPC ENOSPC /* No space left on device */
-#define VACCEL_ENOTDIR ENOTDIR /* Not a directory */
-#define VACCEL_EROFS EROFS /* Read-only file system */
-#define VACCEL_EACCES EACCES /* Permission denied */
-#define VACCEL_EBADF EBADF /* Bad file number */
-#define VACCEL_EREMOTEIO EREMOTEIO /* Remote I/O error */
-#define VACCEL_EFAULT EFAULT /* Bad address */
-#define VACCEL_ERANGE ERANGE /* Out of range */
-#define VACCEL_EAGAIN EAGAIN /* Resource temporarily unavailable */
