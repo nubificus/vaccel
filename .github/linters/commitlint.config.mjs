a4fd167872e6e66b4bfc96fd6d5ea4d923e5a9b7
Kostis Papazafeiropoulos <papazof@gmail.com>
2026-02-23 06:49:42 +0000

    ci(commitlint): Tighten commit message rules
    Tighten commitlint rules for commit message formatting:
    - Enforce sentence case for commit subjects
    - Exclude common git trailers from the max body line length rule, as
      they can easily exceed it and shouldn't be restricted
    
    PR: https://github.com/nubificus/vaccel/pull/210
    Signed-off-by: Kostis Papazafeiropoulos <papazof@gmail.com>
    Reviewed-by: Anastassios Nanos <ananos@nubificus.co.uk>
    Approved-by: Anastassios Nanos <ananos@nubificus.co.uk>

--- .github/linters/commitlint.config.mjs
+++ .github/linters/commitlint.config.mjs
@@ -1,13 +1,43 @@
 export default {
-	extends: ['@commitlint/config-conventional'],
-	helpUrl: 'https://www.conventionalcommits.org/',
-	ignores: [
-		(msg) => /Signed-off-by: dependabot\[bot]/m.test(msg),
-	],
-	rules: {
-		'header-max-length': [2, 'always', 72],
-		'body-max-line-length': [2, 'always', 80],
-		'subject-case': [2, 'never', ['start-case', 'pascal-case', 'upper-case']],
-		'trailer-exists': [2, 'always', 'Signed-off-by:'],
-	},
+  extends: ['@commitlint/config-conventional'],
+  helpUrl: 'https://www.conventionalcommits.org/',
+  ignores: [
+    (msg) => /Signed-off-by: dependabot\[bot]/m.test(msg),
+  ],
+  plugins: [
+    {
+      rules: {
+        'body-max-line-length': (parsed, when, value) => {
+          const { body } = parsed;
+          if (!body) return [true, ''];
+
+          const trailerPattern = /^(Signed-off-by|Co-authored-by|Reviewed-by|Approved-by):/;
+          const violation = body
+            .split('\n')
+            .find((line) => line.length > value && !trailerPattern.test(line));
+
+          return [
+            !violation,
+            violation
+              ? `body's lines must not be longer than ${value} characters (found: "${violation}")`
+              : '',
+          ];
+        },
+      },
+    },
+  ],
+  rules: {
+    'header-max-length': [2, 'always', 72],
+    'body-max-line-length': [2, 'always', 80],
+    'subject-case': [2, 'never', [
+        "lower-case", // lower case
+        "upper-case", // UPPERCASE
+        "camel-case", // camelCase
+        "kebab-case", // kebab-case
+        "pascal-case", // PascalCase
+        "snake-case", // snake_case
+        "start-case", // Start Case
+    ]],
+    'trailer-exists': [2, 'always', 'Signed-off-by:'],
+  },
 }
